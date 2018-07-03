8915a905d301ec0d016c005d9457d916eb4c9938
maskleo <masque@aliyun.com>
2018-07-03 22:58:05 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-keep-alive
+
+https://www.cnblogs.com/wwzyy/p/9104151.html
+
+https://blog.csdn.net/xiaoliuliu2050/article/details/54694461
+
+http://wiselyman.iteye.com/blog/2411933
---
+++ sse/src/main/java/edu/maskleo/sse/SseApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseApplication.class, args);
+    }
+}
